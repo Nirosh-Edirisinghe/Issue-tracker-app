43cbf2a20a749cf946baf7320e639da4a8b01205
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2026-02-12 19:19:34 +0530

    chnage server file

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,13 @@ connectDb()
 
 //middleware
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: "https://issue-flow-ebon.vercel.app/",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 // api endpoint
 app.use('/api/auth',authRouter)
